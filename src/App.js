663add7ab67b5f802813ce7030dbbdf990f32f99
berkayyildirim <berkay.yildirim@yahoo.com>
2021-03-28 21:39:15 +0300

    build sidebar component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react"
 import './App.css';
 import Header from "./Components/Header"
+import Sidebar from "./Components/Sidebar"
 
 function App() {
   return (
     <div className="App">
       <Header />
-        {/* side bar */}
+        <Sidebar />
         {/* feed */}
         {/* widgets */}
     </div>
